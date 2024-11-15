cc291f7d0e1c6e88c974a12028225d50bfc604d0
prakash <p.prakashpaudel35@gmail.com>
2024-11-15 10:35:04 +0530

    the navbar session logout and login changes

--- ER-DIGRAM/src/scomponents/Navbar.jsx
+++ ER-DIGRAM/src/scomponents/Navbar.jsx
@@ -7,21 +7,23 @@ const Navbar = ({ isLoggedIn, handleLogout }) => (
   <div className="navbar-container">
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <a className="navbar-brand" href="/">DrawER</a>
-      <div className="collapse navbar-collapse justify-content-center">
-        <ul className="navbar-nav">
+      <div className="collapse navbar-collapse justify-content-between">
+        <ul className="navbar-nav mx-auto">
           <li className="nav-item">
-            <Link style={{ cursor: "pointer" }}  className="nav-link" to="features" smooth={true} duration={250}>Features</Link>  {/* Use Link for smooth scroll */}
+            <Link style={{ cursor: "pointer" }} className="nav-link" to="features" smooth={true} duration={250}>Features</Link> {/* Use Link for smooth scroll */}
           </li>
           <li className="nav-item">
-            <Link style={{ cursor: "pointer" }}  className="nav-link" to="pricing" smooth={true} duration={250}>Pricing</Link>
+            <Link style={{ cursor: "pointer" }} className="nav-link" to="pricing" smooth={true} duration={250}>Pricing</Link>
           </li>
           <li className="nav-item">
             <a className="nav-link" href="/templates">Templates</a>
           </li>
+        </ul>
+        <ul className="navbar-nav">
           {isLoggedIn ? (
             <li className="nav-item nav-link btn btn-link d-flex align-items-center"
-                onClick={handleLogout}>
-                Logout
+              onClick={handleLogout}>
+              Logout
             </li>
           ) : (
             <>
